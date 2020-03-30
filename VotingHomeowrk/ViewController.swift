18375a3e1cdd30ddc9573f7c052c843cefdee49b
Samuel Hartman <s.hartman@setonhill.edu>
2020-03-30 17:25:36 -0400

    Initial Commit

---
+++ VotingHomeowrk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VotingHomeowrk
+//
+//  Created by Sam Hartman on 3/30/20.
+//  Copyright © 2020 Sam Hartman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
